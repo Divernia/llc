95696d716f0f328d538e93616f89901d35d91e0c
Divernia <1184713814@qq.com>
2018-02-22 16:47:12 +0800

    first commit

---
+++ test-app1/src/main/java/priv/divernia/test/TestApp1Application.java
@@ -0,0 +1,12 @@
+package priv.divernia.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApp1Application.class, args);
+	}
+}
---
+++ test-app2/src/main/java/priv/divernia/test/TestApp2Application.java
@@ -0,0 +1,12 @@
+package priv.divernia.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApp2Application.class, args);
+	}
+}
